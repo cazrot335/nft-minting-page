3fe450c7fdb360f3ec3b3eb6e7069c45e31bfb97
internet_trash <naiknischal92@gmail.com>
2024-01-16 17:51:08 +0530

    fixed contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,28 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require('dotenv');
+dotenv.config();
+
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0"
+      },
+      {
+        version: "0.8.20"
+      }
+    ]
+  },
+  networks: {
+    sepolia: {
+      url: process.env.REACTAPP_RPC_URL,
+      accounts: [process.env.REACTAPP_PRIVATEKEYS]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.REACTAPP_ETHERSCAN_API_KEY
+  },
 };
